30c910e5f79c1d0d8eb17044e719888acc23d2c0
DiFFtY <diffty@gmail.com>
2020-12-14 18:53:05 +0100

    events.js: aded game events + fixed timer not refreshed

--- SRC/js/events.js
+++ SRC/js/events.js
@@ -2,9 +2,6 @@ import { GameSystem } from './gamesystem.js'
 import { CONFIG } from '../config.js'
 
 
-// VARS
-let then = 0;
-
 var gameSystem = new GameSystem();
 
 
@@ -64,17 +61,35 @@ eventSource.onmessage = (message) => {
                             outputString(`${data.content.playerName} est mort :(`)
                         }
                         else {
-                            outputString(`${data.content.playerName} n'est plus mort!`)
+                            outputString(`${data.content.playerName} n'est plus mort !`)
                         }
                     break;
 
                     case "oxygen":
                         let oxygenDelta = data.content.oxygen - localPlayer.oxygen;
                         if (oxygenDelta > 0) {
-                            outputString(`${data.content.playerName} gagne +${oxygenDelta} points d'oxygène!`)
+                            outputString(`${data.content.playerName} gagne +${oxygenDelta} points d'oxygène !`)
                         }
                         else {
-                            outputString(`${data.content.playerName} perd ${oxygenDelta} points d'oxygène!`)
+                            outputString(`${data.content.playerName} perd ${oxygenDelta} points d'oxygène !`)
+                        }
+                    break;
+                }
+            }
+        }
+    }
+    else if (data.type == "game") {
+        let localGame = gameSystem.game;
+
+        for (var i in data.content) {
+            if (localGame[i] != data.content[i]) {
+                switch (i) {
+                    case "alarm":
+                        if (data.content[i] == true) {
+                            outputString(`Catastrophe en cours !`)
+                        }
+                        else {
+                            outputString(`Fin de la catastrophe.`)
                         }
                     break;
                 }
@@ -84,3 +99,20 @@ eventSource.onmessage = (message) => {
 
     gameSystem.receiveEvent(message);
 };
+
+
+// VARS
+let then = 0;
+
+function animate(now) {
+    now *= 0.001;
+    
+    const deltaTime = now - then;
+    then = now;
+
+    gameSystem.update(deltaTime);
+
+    requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
